05709fb5785078b54a1e2b0b906b1459a5ad9d23
Ankit Prajapati <ankitpjlko143@gmail.com>
2024-11-22 09:08:22 +0000

    done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -20,3 +20,20 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
+
+// ******************** Operations ********************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+let str1 = "Hello"
+let str2 = " Ankit"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(+true)
+console.log(+"")
